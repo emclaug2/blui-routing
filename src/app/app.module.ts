fc867579aa8fc639906d141c7dc47813b0907e0c
Evan <evan.w.mclaughlin@gmail.com>
2022-02-08 08:29:35 -0500

    laskdjlakjsgd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,58 +1,25 @@
-/**
- Copyright (c) 2021-present, Eaton
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
- All rights reserved.
-
- This code is licensed under the BSD-3 license found in the LICENSE file in the root directory of this source tree and at https://opensource.org/licenses/BSD-3-Clause.
- **/
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { RouterModule } from '@angular/router';
-
-//brightlayer-ui modules
-import { DrawerModule, EmptyStateModule } from '@brightlayer-ui/angular-components';
-
-//material modules
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-
-// app
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app.routing';
-import { PxbLogoComponent } from './components/pxb-logo/pxb-logo.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { HomeComponent } from './pages/home/home.component';
-import { PageTwoComponent } from './pages/page-two/page-two.component';
-import { PageOneComponent } from './pages/page-one/page-one.component';
+import {MaterialExampleModule} from '../material.module';
+import {ButtonOverviewExample} from './button-overview-example';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        HomeComponent,
-        NavigationComponent,
-        PageOneComponent,
-        PageTwoComponent,
-        PxbLogoComponent,
-    ],
-    imports: [
-        AppRoutingModule,
-        BrowserAnimationsModule,
-        BrowserModule,
-        CommonModule,
-        FormsModule,
-        RouterModule,
-        FlexLayoutModule,
-        MatToolbarModule,
-        MatButtonModule,
-        MatIconModule,
-        DrawerModule,
-        EmptyStateModule,
-    ],
-    bootstrap: [AppComponent],
+  declarations: [ButtonOverviewExample],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    MaterialExampleModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [ButtonOverviewExample],
 })
 export class AppModule {}
